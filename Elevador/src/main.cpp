3e57f528a88af594836b577c448798b84a40488b
PedroGrossi <ph02grossi@gmail.com>
2024-06-27 19:53:12 -0300

    Inicio projeto

--- Elevador/src/main.cpp
+++ Elevador/src/main.cpp
@@ -3,9 +3,15 @@ Alunos = Gabriel Passos e Pedro Henrique Grossi da Silva
 Data = 20/06/2024
 Desenvolvido para a placa Wemos LOLIN32 LITE utilizando ISR / Platformio / FreeRTOS
 ########################################################################################*/
+/* Biblioteca do Arduino */
 #include <Arduino.h>
+
+/* Bibliotecas FreeRTOS */
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+/* Mapeamento de Pinos */
+#define ledDebug 22
 
 struct elevador
 {
@@ -26,15 +32,30 @@ struct elevador
 //var hw serial:
 char rx[13],tx[4],last;
 //contadores genÃ©ricos:
-char i,j;
+int i;
+
 //contadores de eventos:
 char n_btc_in,n_seq_up,n_seq_down;
 
 //Protótipo de funções
 void zerar_serial(void);
 
+/* Variáveis para armazenamento do handle das tasks */
+TaskHandle_t task1Handle = NULL;
+TaskHandle_t InitializedHandle = NULL;
+
+/* Protótipos das Tasks */
+void vTask1(void *pvParameters);
+void vInitialized(void *pvParameters);
+
 void setup() {
+  /* led to debug*/
+  pinMode(ledDebug, OUTPUT);
+  digitalWrite(ledDebug,LOW);
+
   zerar_serial();
+  Serial.begin(115200);
+
   //Zerar elevador na inicialização
   esquerdo.andar=0;
   esquerdo.estado='P';
@@ -46,12 +67,27 @@ void setup() {
   for (i=15;i>=0;i--) esquerdo.btc_down[i]=0;
   for (i=15;i>=0;i--) esquerdo.btc_up[i]=0;
   for (i=14;i>=0;i--) esquerdo.seq_up[i]=0;
-  for (i=14;i>=0;i--) esquerdo.seq_down[i]=0;
-  /* configura comunicação serial com baudrate de 115200 */
-  Serial.begin(115200);
+  for (i=14;i>=0;i--) esquerdo.seq_down[i]=0; 
+
+  /* criação da variável de retorno da criação da task */
+  BaseType_t xReturned;
+
+  /* Criação das Tasks com maior prioridade para forçar troca de contexto para ela na ISR */
+  xReturned = xTaskCreate(vTask1,"Task1",configMINIMAL_STACK_SIZE+512,NULL,1,&task1Handle);
+  if (xReturned == pdFAIL)
+     {
+      Serial.println("Não foi possível criar a Task 1!");
+      while(1);
+     }
+  xReturned = xTaskCreate(vInitialized,"InitializedTask",configMINIMAL_STACK_SIZE,NULL,1,&InitializedHandle);
+  if (xReturned == pdFAIL)
+     {
+      Serial.println("Não foi possível criar a InitializedTask!");
+      while(1);
+     }  
 }
 
-/* loop-Task0 => inverte LED em intervalos de 333ms */
+/* loop-Task0 */
 void loop()
 {
   //Verifica se existe algum byte na porta serial, caso exista rotaciona buffer e inclui o ultimo byte recebido.
@@ -61,77 +97,48 @@ void loop()
     for (i=0;i<12;i++) rx[i]=rx[i+1];
     rx[i]=last;
   }
-  //Recebeu fim de string do SIMSE2 -> tratar mensagem recebida:
-  if (rx[11]=='\r'&&rx[12]=='\n')
-  {
-    //SIMSE2 -> Enviando msg de inicializaÃ§Ã£o:
-    if (rx[0]=='i'&&rx[1]=='n'&&rx[2]=='i'&&rx[3]=='t'&&rx[4]=='i'&&rx[5]=='a'&&rx[6]=='l'&&rx[7]=='i'&&rx[8]=='z'&&rx[9]=='e'&&rx[10]=='d'&&rx[11]=='\r'&&rx[12]=='\n')
-    {
-      //Envia msg resete elevador esquerdo - delay para esperar o SIMSE2 abrir:
-      delay(4000);//delay para SIMSE2 abrir: varia de PC para PC (1000-4000). SUPER Loop: Sem delay no programa principal ... aqui nao afeta o funcionamento apos init ...
-      tx[0]='e';tx[1]='r';tx[2]='\r';
-      Serial.write(tx,3);
-      esquerdo.andar=0;
-      esquerdo.estado='P';
-      esquerdo.prox=0;
-      esquerdo.n_btc_in=0;
-      esquerdo.n_seq_up=0;
-      esquerdo.n_seq_down=0;
-      for (i=15;i>=0;i--) esquerdo.btc_in[i]=0;
-      for (i=15;i>=0;i--) esquerdo.btc_down[i]=0;
-      for (i=15;i>=0;i--) esquerdo.btc_up[i]=0;
-      for (i=15;i>=0;i--) esquerdo.seq_up[i]=0;
-      for (i=15;i>=0;i--) esquerdo.seq_down[i]=0;
-      zerar_serial();
-    }
-    //SIMSE2 -> Enviando msg porta aberta:
-    if (rx[9]=='e'&&rx[10]=='A'&&rx[11]=='\r'&&rx[12]=='\n')
-    {
-      esquerdo.porta=0;
-      zerar_serial();
-    }
-    //SIMSE2 -> Enviando msg porta fechada:
-    if (rx[9]=='e'&&rx[10]=='F'&&rx[11]=='\r'&&rx[12]=='\n')
-    {
-      if (esquerdo.estado=='S')
-      {
-        tx[0]='e';tx[1]='s';tx[2]='\r';
-        Serial.write(tx,3);
-      }
-      if (esquerdo.estado=='D')
-      {
-        tx[0]='e';tx[1]='d';tx[2]='\r';
-        Serial.write(tx,3);
-      }
-      esquerdo.porta=1;
-      zerar_serial();
-    }
+  vTaskDelay(pdMS_TO_TICKS(200));
+}
 
+/* vTask1 => inverte LED em intervalos de 200 ms */
+void vTask1(void *pvParameters)
+{
+  while (1)
+        {
+          digitalWrite(ledDebug,!digitalRead(ledDebug));
+          vTaskDelay(pdMS_TO_TICKS(200));
+        }
+}
 
-    //SIMSE2 -> Enviando msg BotÃ£o corredor terreo sobe:
-    if (rx[6]=='e'&&rx[7]=='E'&&rx[8]=='0'&&rx[9]=='0'&&rx[10]=='s'&&rx[11]=='\r'&&rx[12]=='\n')
-    {
-      if (esquerdo.andar!=0) esquerdo.btc_up[0]=1;                              //NÃ£o ascender botÃ£o no mesmo andar ...
-          
-      //Envia msg ascender luz do botÃ£o 0 central como DEBUG ...
-      tx[0]='c';tx[1]='L';tx[2]='a';tx[3]='\r';Serial.write(tx,4);
-          
-      n_seq_up=0;                                                               //reset contadores de eventos ...
-      zerar_serial();
-    }
-    //SIMSE2 -> Enviando msg BotÃ£o corredor 1 andar sobe:
-    if (rx[6]=='e'&&rx[7]=='E'&&rx[8]=='0'&&rx[9]=='1'&&rx[10]=='s'&&rx[11]=='\r'&&rx[12]=='\n')
-    {
-      if (esquerdo.andar!=1) esquerdo.btc_up[1]=1;                    //NÃ£o ascender botÃ£o no mesmo andar ...
-          
-      //Envia msg ascender luz do botÃ£o 0 central como DEBUG ...
-      tx[0]='c';tx[1]='L';tx[2]='b';tx[3]='\r';Serial.write(tx,4);
-         
-      n_seq_up=0;                                                               //reset contadores de eventos ...
-      zerar_serial();
-    }
-  }
-  vTaskDelay(pdMS_TO_TICKS(1000));
+/* vTask1 => inverte LED em intervalos de 200 ms */
+void vInitialized(void *pvParameters)
+{
+  while (1)
+        {
+          //SIMSE2 -> Enviando msg de inicialização:
+          if (rx[0]=='i'&&rx[1]=='n'&&rx[2]=='i'&&rx[3]=='t'&&rx[4]=='i'&&rx[5]=='a'&&rx[6]=='l'&&rx[7]=='i'&&rx[8]=='z'&&rx[9]=='e'&&rx[10]=='d'&&rx[11]=='\r'&&rx[12]=='\n')
+          {
+            //Envia msg resete elevador esquerdo - delay para esperar o SIMSE2 abrir:
+            delay(5000);//delay para SIMSE2 abrir: varia de PC para PC (1000-4000). SUPER Loop: Sem delay no programa principal ... aqui nao afeta o funcionamento apos init ...
+            tx[0]='e';tx[1]='r';tx[2]='\r';
+            Serial.write(tx,3);
+            esquerdo.andar=0;
+            esquerdo.estado='P';
+            esquerdo.prox=0;
+            esquerdo.n_btc_in=0;
+            esquerdo.n_seq_up=0;
+            esquerdo.n_seq_down=0;
+            for (i=15;i>=0;i--) esquerdo.btc_in[i]=0;
+            for (i=15;i>=0;i--) esquerdo.btc_down[i]=0;
+            for (i=15;i>=0;i--) esquerdo.btc_up[i]=0;
+            for (i=15;i>=0;i--) esquerdo.seq_up[i]=0;
+            for (i=15;i>=0;i--) esquerdo.seq_down[i]=0;
+            zerar_serial();
+            /*Se auto exclui*/
+            vTaskDelete(InitializedHandle);
+          }
+          vTaskDelay(pdMS_TO_TICKS(200));
+        }
 }
 
 void zerar_serial(void)
