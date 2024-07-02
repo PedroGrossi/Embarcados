6a6254e97edeb479b6b7f44010862ec7f23e054f
PedroGrossi <ph02grossi@gmail.com>
2024-07-01 21:13:53 -0300

    Removendo variavel e condição não utilizadas

--- Elevador/lib/elevatorController/src/elevatorController.cpp
+++ Elevador/lib/elevatorController/src/elevatorController.cpp
@@ -10,28 +10,24 @@ Desenvolvido para a placa Wemos LOLIN32 LITE utilizando Ambiente ARDUINO
 #include "..\..\..\src\main.h"
 
 
-void controller(char *tx, unsigned long timerelevador, struct elevador *esquerdo)
+void controller(char *tx, struct elevador *esquerdo)
 {
-    if (millis()>timerelevador)
-   {
-      timerelevador=millis();
-      if(esquerdo->andar<esquerdo->prox)
-      {
-        /* Elevador sobe */
-        esquerdo->estado='S';
-      }
-      if (esquerdo->andar>esquerdo->prox)
-      {
-        /* Elevador desce */
-        esquerdo->estado='D';
-      }
-      if (esquerdo->estado!='P')
-      {
-        /* Fecha a porta do elevador*/
-        esquerdo->porta=1;
-        tx[0]='e';tx[1]='f';tx[2]='\r';
-        Serial.write(tx,3);
-        zerar_serial();
-      }
-   }
+  if(esquerdo->andar<esquerdo->prox)
+  {
+    /* Elevador sobe */
+    esquerdo->estado='S';
+  }
+  if (esquerdo->andar>esquerdo->prox)
+  {
+    /* Elevador desce */
+    esquerdo->estado='D';
+  }
+  if (esquerdo->estado!='P')
+  {
+    /* Fecha a porta do elevador*/
+    esquerdo->porta=1;
+    tx[0]='e';tx[1]='f';tx[2]='\r';
+    Serial.write(tx,3);
+    zerar_serial();
+  }
 }
