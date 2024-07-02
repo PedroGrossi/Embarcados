8a23c1da534c841c4950e9e43bc05d618658f266
PedroGrossi <ph02grossi@gmail.com>
2024-07-01 21:14:33 -0300

    Ajustado variavel de tempo de espera em cada andar

--- Elevador/lib/elevatorResponse/src/elevatorResponse.h
+++ Elevador/lib/elevatorResponse/src/elevatorResponse.h
@@ -13,7 +13,7 @@ simulador de elevador
 
 void initialized(char *rx, char *tx, struct elevador *esquerdo);
 void doorStatus(char *rx, char *tx, struct elevador *esquerdo);
-void floorVerify(char *rx, char *tx, char n_btc_in, char n_seq_up, unsigned long timerelevador, struct elevador *esquerdo);
+void floorVerify(char *rx, char *tx, char n_btc_in, char n_seq_up, unsigned long *timerelevador, struct elevador *esquerdo);
 void cabinButton(char *rx, char*tx, char n_btc_in, struct elevador *esquerdo);
 void hallwayUpButton(char *rx, char *tx, char n_seq_up, struct elevador *esquerdo);
 void hallwayDownButton(char *rx, char *tx, char n_seq_down, struct elevador *esquerdo);
